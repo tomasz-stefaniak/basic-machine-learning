add07c0c81e103b8492fd2a2aa59a780896b712b
tomasz-io <tomasz.p.stefaniak@gmail.com>
2022-12-28 13:16:49 +0100

    clean up clothing code

--- clothing.py
+++ clothing.py
@@ -8,30 +8,65 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images,
                                test_labels) = fashion_mnist.load_data()
 
+
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-# # UNCOMMENT THIS TO VIEW A SINGLE IMAGE
-# IMAGE_INDEX = 0
-# plt.figure()
-# plt.imshow(train_images[IMAGE_INDEX])
-# plt.colorbar()
-# plt.show()
+PIXEL_VALUE_RANGE = 255.0
 
 
-# # UNCOMMENT THIS TO VIEW IMAGES WITH LABELS
-# NUMBER_OF_ROWS = 5
-# NUMBER_OF_COLUMNS = 5
-# NUMBER_OF_IMAGES = 25
-# FIGURE_SIZE = (9, 9)
-# plt.figure(figsize=FIGURE_SIZE)
-# for index in range(NUMBER_OF_IMAGES):
-#     plt.subplot(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS, index + 1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.imshow(train_images[index], cmap=plt.cm.binary)
-#     plt.xlabel(class_names[train_labels[index]])
-# plt.show()
+def display_single_image():
+    image_index = 0
+    plt.figure()
+    plt.imshow(train_images[image_index])
+    plt.colorbar()
+    plt.show()
 
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+
+def display_images_with_labels():
+    number_of_rows = 5
+    number_of_columns = 5
+    number_of_images = 25
+    figure_size = (9, 9)
+    plt.figure(figsize=figure_size)
+    for index in range(number_of_images):
+        plt.subplot(number_of_rows, number_of_columns, index + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(train_images[index], cmap=plt.cm.binary)
+        plt.xlabel(class_names[train_labels[index]])
+    plt.show()
+
+
+def setup_model(train_images, train_labels, test_images, test_labels):
+    train_images = train_images / PIXEL_VALUE_RANGE
+    test_images = test_images / PIXEL_VALUE_RANGE
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                      from_logits=True),
+                  metrics=['accuracy'])
+
+    model.fit(train_images, train_labels, epochs=10)
+
+    test_loss, test_accuracy = model.evaluate(
+        test_images,  test_labels, verbose=2)
+
+    print('\nTest accuracy:', test_accuracy)
+
+    probability_model = tf.keras.Sequential([model,
+                                            tf.keras.layers.Softmax()])
+    predictions = probability_model.predict(test_images)
+
+    print(np.argmax(predictions[0]))
+
+
+display_single_image()
+display_images_with_labels()
+setup_model(train_images, train_labels, test_images, test_labels)
