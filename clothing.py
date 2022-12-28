46ac36e001ee26f8390aac1e4e6e59a08492216b
tomasz-io <tomasz.p.stefaniak@gmail.com>
2022-12-28 13:25:49 +0100

    update clothing model

--- clothing.py
+++ clothing.py
@@ -53,20 +53,62 @@ def setup_model(train_images, train_labels, test_images, test_labels):
                       from_logits=True),
                   metrics=['accuracy'])
 
-    model.fit(train_images, train_labels, epochs=10)
+    model.fit(train_images, train_labels, epochs=2)
 
-    test_loss, test_accuracy = model.evaluate(
+    _, test_accuracy = model.evaluate(
         test_images,  test_labels, verbose=2)
 
     print('\nTest accuracy:', test_accuracy)
 
     probability_model = tf.keras.Sequential([model,
                                             tf.keras.layers.Softmax()])
-    predictions = probability_model.predict(test_images)
 
-    print(np.argmax(predictions[0]))
+    return probability_model
 
 
-display_single_image()
-display_images_with_labels()
-setup_model(train_images, train_labels, test_images, test_labels)
+def plot_image(index, predictions_array, true_label, img):
+    true_label, img = true_label[index], img[index]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100*np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(index, predictions_array, true_label):
+    true_label = true_label[index]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+# display_single_image()
+# display_images_with_labels()
+model = setup_model(train_images, train_labels, test_images, test_labels)
+
+predictions = model.predict(test_images)
+
+i = 20
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions[i],  test_labels)
+plt.show()
